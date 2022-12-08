78e6d14d2f47a47ec21ddf9488cc93fab7ae3ec6
Pedro <pedrohalmeidamendonca@gmail.com>
2022-12-08 17:04:55 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../redux/actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleSubmit = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(login(email));
+    history.push('/carteira');
+  };
+
   render() {
+    const { email, password } = this.state;
+    const minLength = 6;
+
     return (
       <div>
-        <h1>Login</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            data-testid="email-input"
+            onChange={ ({ target: { value } }) => this.setState({ email: value }) }
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            data-testid="password-input"
+            onChange={ ({ target: { value } }) => this.setState({ password: value }) }
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            disabled={ !/\S+@\S+\.\S+/.test(email) || password.length < minLength }
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
