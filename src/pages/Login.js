be1640a99ef86e11b42b36a255df65a0c5f70534
Pedro <pedrohalmeidamendonca@gmail.com>
2022-12-08 14:52:25 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
   }
 }
 
